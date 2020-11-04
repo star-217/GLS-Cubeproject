cd98cd1c0f7b9d53df2d12042efd15a91de3412f
rinnderu0505@gmail.com <rinnderu0505@gmail.com>
2020-11-04 19:37:38 +0900

    パーティクルで色を塗る

---
+++ Cube Paint/Assets/sasakiFolder/sasaki_Script/bullon_hit_Script.cs
@@ -0,0 +1,44 @@
+﻿using Es.InkPainter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullon_hit_Script : MonoBehaviour
+{
+    [SerializeField]
+    private Brush brush = null;
+
+    private ParticleSystem part;
+    private List<ParticleCollisionEvent> collisionEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // GetComponent<MeshRenderer>().material.color = brush.Color;
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        
+       
+        var canvas = other.GetComponent<InkCanvas>();
+
+        if (canvas != null)
+        {
+            int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+            for (int i = 0; i < numCollisionEvents; i++)
+            {
+                canvas.Paint(brush, collisionEvents[i].intersection);
+            }
+
+        }
+        
+    }
+}
