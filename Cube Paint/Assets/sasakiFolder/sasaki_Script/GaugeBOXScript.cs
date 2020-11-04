b16c14ffcc5147ffc966dc7e962a47c36e3c8209
rinnderu0505@gmail.com <rinnderu0505@gmail.com>
2020-11-04 17:51:29 +0900

    ゲージのバー実装

---
+++ Cube Paint/Assets/sasakiFolder/sasaki_Script/GaugeBOXScript.cs
@@ -0,0 +1,45 @@
+﻿using Es.InkPainter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeBOXScript : MonoBehaviour
+{
+    public GameObject inkCanvas_obj;
+    private InkCanvas inkCanvas;
+
+    [Header("ImageGaugeを入れる")]
+    public Image cooldown;
+    private float gauge_animation;
+    private bool gaugeAnimation_control;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gaugeAnimation_control = false;
+        gauge_animation = 0.001f;
+        cooldown.fillAmount = 0;
+        inkCanvas = inkCanvas_obj.GetComponent<InkCanvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown.fillAmount <= inkCanvas.Per / 100)
+        {
+            gaugeAnimation_control = true;
+        }
+
+        if (gaugeAnimation_control)
+        {
+            cooldown.fillAmount += gauge_animation;
+            if (cooldown.fillAmount >= inkCanvas.Per / 100)
+            {
+                cooldown.fillAmount = inkCanvas.Per / 100;
+                gaugeAnimation_control = false;
+            }
+        }
+    }
+}
