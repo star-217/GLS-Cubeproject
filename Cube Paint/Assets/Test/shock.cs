7e57535f65187fa0d2bb44992999e7d1b409c835
星寛文 <>
2020-11-05 13:36:32 +0900

    壁に当たった時にパーティクルが出るように

---
+++ Cube Paint/Assets/Test/shock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shock : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Instantiate(particle, collision.gameObject.transform.position, Quaternion.identity);
+    }
+}
