78e57d393a433ea7bfaf2efd623b94954e64ab2c
星寛文 <>
2020-11-12 18:06:19 +0900

    画像追加

---
+++ Cube Paint/Assets/Test/NextScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    Button testButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testButton = GetComponent<Button>();
+        testButton.onClick.AddListener(OnclickScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnclickScene()
+    {
+        SceneManager.LoadScene("stage2");
+    }
+}
---
+++ Cube Paint/Assets/Test/Reset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    Button testButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testButton = GetComponent<Button>();
+        testButton.onClick.AddListener(Onclick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Onclick()
+    {
+        SceneManager.LoadScene("stage1");
+    }
+}
