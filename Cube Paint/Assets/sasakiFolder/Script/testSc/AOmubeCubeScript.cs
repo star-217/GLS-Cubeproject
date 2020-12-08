40ae7e49891427bd9e1a5956c489bb71ebf30764
rinnderu0505@gmail.com <rinnderu0505@gmail.com>
2020-12-08 19:54:23 +0900

    ステージ追加作業中

---
+++ Cube Paint/Assets/sasakiFolder/Script/testSc/AOmubeCubeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOmubeCubeScript : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0.05f;
+    [SerializeField]
+    float max_x = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed, 0, 0);
+        if (transform.position.x > max_x || transform.position.x < -max_x)
+        {
+            speed *= -1;
+        }
+    }
+}
