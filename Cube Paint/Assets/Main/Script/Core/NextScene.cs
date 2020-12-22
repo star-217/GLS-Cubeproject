d826380bf5dbf7479dae846834a991addd4f156f
rinnderu0505@gmail.com <rinnderu0505@gmail.com>
2020-12-22 09:09:00 +0900

    バウンド系いじった。 サブボールが落ちてくるスクリプトを作ろうと思った。

--- Cube Paint/Assets/Main/Script/Core/NextScene.cs
+++ Cube Paint/Assets/Main/Script/Core/NextScene.cs
@@ -58,7 +58,7 @@ public class NextScene : MonoBehaviour
 
         GLS.GLSAnalyticsUtility.TrackEvent("StageClear", "Stage" + stage, stage);
         stage += 1;
-        if (stage > 50)
+        if (stage > 18)
         {
             stage = 1;
         }
---
+++ Cube Paint/Assets/sasakiFolder/Script/subBollDownScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subBollDownScript : MonoBehaviour
+{
+    private Rigidbody myRigidbody; //サブボールのRigidbody
+    private Vector3   myPosition;  //サブボールのPosition
+    //private float subBall_down = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+        myPosition  = gameObject.GetComponent<Transform>().position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if(myPosition.y <= 0.5f)
+        //{
+        //    myRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        //}
+        //subBall_down -= 0.01f;
+        //transform.position = new Vector3(0, subBall_down, 0);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "subBallstopTag")
+        {
+            myRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+}
