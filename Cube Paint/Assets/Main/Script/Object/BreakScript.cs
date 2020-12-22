278a9c8d6133f63e3f601c6a50f05a9c7c3abe77
rinnderu0505@gmail.com <rinnderu0505@gmail.com>
2020-12-22 19:02:31 +0900

    壁エフェクトでてくる

--- Cube Paint/Assets/Main/Script/Object/BreakScript.cs
+++ Cube Paint/Assets/Main/Script/Object/BreakScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class BreakScript : MonoBehaviour
 {
     private ParticleSystem BreakWallParticle;
+    private MeshRenderer myMeshRenderer;
+    private BoxCollider myBoxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         BreakWallParticle = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+        myMeshRenderer = gameObject.GetComponent<MeshRenderer>();
+        myBoxCollider = gameObject.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -21,6 +25,8 @@ public class BreakScript : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            myMeshRenderer.enabled = false;
+            myBoxCollider.enabled = false;
             BreakWallParticle.Play();
             //Destroy(gameObject);
         }
