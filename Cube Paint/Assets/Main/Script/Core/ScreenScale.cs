ba216848530e09446a143bc0a8ec6dd3c3373d4d
星寛文 <>
2021-01-06 16:57:30 +0900

    ショップシーンバグ修正

---
+++ Cube Paint/Assets/Main/Script/Core/ScreenScale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //最初に作った画面のwidth
+    public float defaultWidth = 9.0f;
+
+    //最初に作った画面のheight
+    public float defaultHeight = 16.0f;
+
+    void Start()
+    {
+        //camera.mainを変数に格納
+        Camera mainCamera = Camera.main;
+
+        //最初に作った画面のアスペクト比 
+        float defaultAspect = defaultWidth / defaultHeight;
+
+        //実際の画面のアスペクト比
+        float actualAspect = (float)Screen.width / (float)Screen.height;
+
+        //実機とunity画面の比率
+        float ratio = actualAspect / defaultAspect;
+
+        //サイズ調整
+        mainCamera.orthographicSize /= ratio;
+
+    }
+}
