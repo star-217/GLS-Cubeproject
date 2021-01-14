c2ea194d41425d45e6d26819daf23739f83e7cdd
souta_miura <miura042910@gmail.com>
2021-01-14 16:43:19 +0900

    feat:プレイヤーのエフェクト再生にオブジェクトプールを導入

--- Cube Paint/Assets/Main/Script/ParticleController.cs
+++ Cube Paint/Assets/Main/Script/ParticleController.cs
@@ -7,6 +7,12 @@ public class ParticleController : MonoBehaviour
     [SerializeField] private GameObject[] particles = { null };
 
     private List<ParticleSystem> particlesList = new List<ParticleSystem>();
+    
+    private List<GameObject> poolObjList = new List<GameObject>();
+
+    private int objNum = 0;
+
+    Color particleColor;
 
     void Start()
     {
@@ -20,17 +26,65 @@ public class ParticleController : MonoBehaviour
     [System.Obsolete]
     public void ParticlePlay(int particleNum, Vector3 position, Quaternion rotation)
     {
-        particles[particleNum].transform.position = position;
-        particles[particleNum].transform.rotation = rotation;
-        particlesList[particleNum].Play();
+        var effect = GetObject(particleNum);
+        effect.transform.position = position;
+        effect.transform.rotation = rotation;
     }
 
     [System.Obsolete]
     public void ParticleColor(int particleNum, Color color)
     {
+        CreatePool(particleNum, 1);
         var newColor = new ParticleSystem.MinMaxGradient();
         newColor.mode = ParticleSystemGradientMode.Color;
         newColor.color = color;
-        particlesList[particleNum].startColor = newColor.color;
+        particleColor = newColor.color;
+    }
+
+    [System.Obsolete]
+    void CreatePool(int num, int maxCount) 
+    {
+        for (int i = 0; i < maxCount; ++i)
+        {
+            var newObj = CreateNewObject(num);
+            newObj.SetActive(false);
+            poolObjList.Add(newObj);
+        }
+    }
+
+    [System.Obsolete]
+    public GameObject GetObject(int num)
+    {
+        objNum = 0;
+
+        // 使用中でないものを探して返す
+        foreach (var obj in poolObjList)
+        {
+            if (obj.activeSelf == false)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+            objNum++;
+        }
+
+        // 全て使用中だったら新しく作って返す
+        var newObj = CreateNewObject(num);
+        newObj.SetActive(true);
+        poolObjList.Add(newObj);
+
+        return newObj;
+    }
+
+    [System.Obsolete]
+    private GameObject CreateNewObject(int num)
+    {
+        var newObj = Instantiate(particles[num]);
+        newObj.name = particles[num].name + (poolObjList.Count + 1);
+        var effect = newObj.GetComponent<ParticleSystem>();
+        particlesList.Add(effect);
+        particlesList[objNum].startColor = particleColor;
+
+        return newObj;
     }
 }
