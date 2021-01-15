4aa2d31ca41d80e99455330fb51c37a71f023836
souta_miura <miura042910@gmail.com>
2021-01-15 10:41:03 +0900

    refactor:クオリティ設定変更、Materialの設定変更

---
+++ Cube Paint/Assets/Main/Script/SetFrameRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFrameRate : MonoBehaviour
+{
+    [Header("フレームレートの指定")]
+    [SerializeField] private int frameRateValue = 60;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = frameRateValue;
+    }
+}
